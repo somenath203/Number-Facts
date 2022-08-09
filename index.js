c6d3ad4eb6a254c1f357a6d002f539bc9cd2a518
Somenath Choudhury <somenathchoudhury35@gmail.com>
2022-08-09 19:46:45 +0530

    changed ajax to fetch

---
+++ index.js
@@ -0,0 +1,38 @@
+const fact = document.querySelector('#fact');
+const wrapperFact = document.querySelector('.wrapper-fact');
+const factText = document.querySelector('#factTextBox');
+const numberInput = document.querySelector('#numberInput');
+const numberSpan = document.querySelector('span');
+
+
+numberInput.addEventListener('input', () => {
+
+    const number = numberInput.value;
+
+
+    fetch(`http://numbersapi.com/${number}`)
+        .then(response => response.text())
+        .then(data => {
+
+            if (number === '') {
+
+                fact.style.display = 'none';
+
+                wrapperFact.style.display = 'none';
+
+            }
+
+            if (number !== '') {
+
+                fact.style.display = 'block';
+
+                wrapperFact.style.display = 'block';
+
+                factText.textContent = data;
+
+            }
+        })
+        .catch(err => console.log(err));
+
+});
+
